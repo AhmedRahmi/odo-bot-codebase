9c22a9ed469c50ece730156187b882cca1cc2c50
Broswei <stevehzwei@gmail.com>
2024-06-13 15:33:35 -0400

    goofy ahh op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+
+@Autonomous
+public class TestAuto extends LinearOpMode {
+
+    MecanumDrive drive;
+    Action trajectory;
+
+    @Override
+    public void runOpMode () {
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(0)));
+
+        trajectory = drive.actionBuilder(drive.pose)
+                .lineToY(40)
+                .lineToX(40)
+                .lineToY(0)
+                .lineToX(0)
+                .build();
+
+        while(!isStarted() && !opModeIsActive()) {}
+
+
+        Actions.runBlocking(trajectory);
+    }
+
+}
